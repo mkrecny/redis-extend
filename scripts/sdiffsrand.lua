260f601957bc899a860855c4a7bed3fbe732da4e
Pierre Chapuis <catwell@archlinux.us>
2011-06-01 18:52:48 +0200

    Remove RANDSUBSTORE and SDIFFSRAND, (...)
    ... add SDIFFRANDSUBSET and SRANDSUBSET,
    update README.

--- scripts/sdiffsrand.lua
+++
@@ -1,6 +0,0 @@
-local diff = redis.call("sdiff",unpack(KEYS))
-local max = tonumber(ARGV[1])
-if max > #diff then max = #diff end
-local output = {} 
-for i=0,max-1 do table.insert(output,table.remove(diff)) end
-return output
---
+++ scripts/srandsubset.lua
@@ -0,0 +1,10 @@
+function rand_subset(set,max_size)
+  local limit = #set - max_size + 1
+  if limit < 1 then limit = 1 end
+  local r = {}
+  for i=#set,limit,-1 do
+    r[#r+1] = table.remove(set,math.random(i))
+  end
+  return r
+end
+return rand_subset(redis.call("smembers",KEYS[1]),tonumber(ARGV[1]))
